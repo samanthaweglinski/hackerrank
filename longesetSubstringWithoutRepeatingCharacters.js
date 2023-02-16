855664c0554937332d7c8ac7b1c3c8533736985f
Samantha Weglinski <32751992+samanthaweglinski@users.noreply.github.com>
2023-02-15 16:21:38 -0800

    Create longesetSubstringWithoutRepeatingCharacters.js

---
+++ longesetSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,10 @@
+/*
+ * Complete the 'lengthOfLongestSubstring' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function lengthOfLongestSubstring(s) {
+
+}
