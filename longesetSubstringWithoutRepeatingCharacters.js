1e10eb27e1407ac04f56896394c4db79861c9f53
Samantha Weglinski <32751992+samanthaweglinski@users.noreply.github.com>
2023-02-15 16:50:11 -0800

    Update longesetSubstringWithoutRepeatingCharacters.js

--- longesetSubstringWithoutRepeatingCharacters.js
+++ longesetSubstringWithoutRepeatingCharacters.js
@@ -6,5 +6,18 @@
  */
 
 function lengthOfLongestSubstring(s) {
-
+    let maxLength = 0
+    let left = 0
+    let set = new Set()
+    
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) { // if our set has the next element, we move our left pointer up one
+            set.delete(s[left])
+            left += 1
+        }
+        set.add(s[right])
+        maxLength = Math.max(maxLength, right - left + 1)
+    }
+    
+    return maxLength
 }
